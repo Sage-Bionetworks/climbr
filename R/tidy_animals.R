24aff4674c824527481da4937273d1c975f18f5a
Kara Woo <woo.kara@gmail.com>
2018-07-03 12:27:19 -0700

    Ensure that all fields in Get Animals By Job endpoint are included

--- R/tidy_animals.R
+++ R/tidy_animals.R
@@ -44,8 +44,55 @@ tidy_animals.response <- function(data, include_params = TRUE) {
   tibble::as_tibble(data)
 }
 
+#' Ensure that the data retrieved from the CLIMB API has all the necessary
+#' columns
+#'
+#' If fields have no data, they're not returned by the CLIMB API, which can lead
+#' to missing columns if all the animals returned by a query have missing values
+#' in that field. `xomplete_job_columns()` ensures that all columns are present
+#' (and populated with `NA` if there is no data).
+#'
+#' @param data Output of [tidy_animals()]
+#' @return A data frame containing all columns returned by the CLIMB Get Animals
+#'   By Job API
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' r <- get_animals()
+#' dat <- tidy_animals(r)
+#' complete_job_columns(dat)
+#' }
+#'
+complete_job_columns <- function(data) {
+  data[, setdiff(pkgenv$animals_by_job_headers, names(data))] <- NA
+  data
+}
 
 list_to_animal_df <- function(x) {
   x <- roomba::replace_null(x) # replace NULLs with NAs so they aren't dropped by unlist
   tibble::as_tibble(as.list(unlist(x)))
 }
+
+# Column names that should be present in Get Animals By Job
+pkgenv <- new.env()
+pkgenv$animals_by_job_headers <- c(
+  "CommonName",
+  "LineName",
+  "Identifier",
+  "ExternalIdentifier",
+  "CurrentLocationPath",
+  "AnimalStatus",
+  "Sex",
+  "AnimalName",
+  "DateBorn",
+  "DateExit",
+  "PhysicalMarker",
+  "Comments",
+  "MaterialOrigin",
+  "Generation",
+  "AnimalMatingStatus",
+  "MatingID",
+  "BirthID",
+  "jobNumber"
+)
--- tests/testthat/test_tidy_animals.R
+++ tests/testthat/test_tidy_animals.R
@@ -1,2 +1,15 @@
 context("tidy_animals")
 
+test_that("complete_job_columns returns correct columns", {
+  dat <- data.frame(CommonName = "moomin", jobNumber = 5, AnimalName = "betty")
+  dat_complete <- complete_job_columns(dat)
+  comparison <- setdiff(pkgenv$animals_by_job_headers, names(dat_complete))
+  expect_equal(comparison, character(0))
+})
+
+test_that("complete_job_columns doesn't drop additional columns", {
+  dat <- data.frame(CommonName = "moomin", jobNumber = 5, extraCol = 1)
+  dat_complete <- complete_job_columns(dat)
+  comparison <- setdiff(names(dat_complete), pkgenv$animals_by_job_headers)
+  expect_equal(comparison, "extraCol")
+})
